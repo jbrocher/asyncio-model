2a5220c2c2bcfe2b758d412f61eb39ec7d3be1c4
jb <jeanb.rocher@gmail.com>
2025-04-29 14:35:49 +0200

    done

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,7 +1,9 @@
 from collections import deque
 from typing import Generator
 
+from scheduler.future import Future
 from scheduler.task import Task
+from scheduler.task_protocol import TaskProtocol
 
 
 class Scheduler:
@@ -10,13 +12,27 @@ class Scheduler:
 
     def create_task(self, coro: Generator):
         task = Task(coro)
+        self._schedule(task)
+
+    def _schedule(self, task: TaskProtocol):
         self._queue.append(task)
+
+    def _continue(self, task):
+        def callback(result):
+            task.val = result
+            self._schedule(task)
+
+        return callback
 
     def run_forever(self):
         while self._queue:
             task = self._queue.pop()
             try:
-                task.run()
-                self._queue.appendleft(task)
+                future = task.run()
+                if isinstance(future, Future):
+                    future.add_done_callback(self._continue(task))
+                    self._schedule(future)
+                else:
+                    self._queue.appendleft(task)
             except StopIteration as err:
                 print(err.value)
--- scheduler/task.py
+++ scheduler/task.py
@@ -4,9 +4,9 @@ import uuid
 
 class Task:
     def __init__(self, coro: Generator):
-        self.id = uuid.uuid4()
         self._coro = coro
+        self.id = uuid.uuid4()
         self.val = None
 
     def run(self):
-        self._coro.send(self.val)
+        return self._coro.send(self.val)
